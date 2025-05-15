6ec9de527c6a5caf11bc11a6e3dcd756443e5816
bamboosir920 <bamboosir920@163.com>
2025-05-15 16:01:23 +0800

    local LLM

--- output/005_Step_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-which python
-conda config --set show_channel_urls false
-conda config --add channels conda-forge
-conda config --add channels bioconda
-mkdir -p ./output/005
-conda install -y trimmomatic
-trimmomatic PE -phred33 ./data/rnaseq_1.fastq.gz ./data/rnaseq_2.fastq.gz ./output/005/trimmed_rnaseq_1.fastq.gz ./output/005/unpaired_rnaseq_1.fastq.gz ./output/005/trimmed_rnaseq_2.fastq.gz ./output/005/unpaired_rnaseq_2.fastq.gz ILLUMINACLIP:./data/minigenome.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
